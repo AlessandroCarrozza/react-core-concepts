5a8016314528bec6327822af001b894aab16bc0e
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-12-19 14:35:26 +0100

    dinamic content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// randomic word from reactDescription array
+const description = reactDescription[genRandomInt(reactDescription.length - 1)];
+
 // creation new component
 function Header() {
   return (
     // the () of return if more of 1 line markup
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
