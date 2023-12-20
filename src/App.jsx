675bdc861faa9ca49221fb61be139ee088176afe
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-12-20 15:29:34 +0100

    props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
 // componente principale
@@ -26,6 +27,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          {/* menu tag never used */}
+          <menu>
+            {/* no self closing */}
+            <TabButton>Components</TabButton>
+            <TabButton></TabButton>
+            <TabButton></TabButton>
+            <TabButton></TabButton>
+          </menu>
         </section>
       </main>
     </div>
